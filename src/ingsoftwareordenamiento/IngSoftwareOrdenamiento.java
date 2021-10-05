e3fe81c4cfebb661a97f7e41440a34be622d64a1
xCyberInk <81275468+xCyberInk@users.noreply.github.com>
2021-10-05 15:54:13 -0500

    Agregando Proyecto

---
+++ src/ingsoftwareordenamiento/IngSoftwareOrdenamiento.java
@@ -0,0 +1,35 @@
+package ingsoftwareordenamiento;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JorgeLPR
+ */
+public class IngSoftwareOrdenamiento extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ViewLogin.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
